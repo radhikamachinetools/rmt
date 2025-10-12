01f3967992c7a8d352b54352685395710374e17d
ashokverma-an <ashokverma.an.2001@gmail.com>
2025-10-12 15:41:00 +0530

    done by ashok verm

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const bcrypt = require('bcryptjs');
 import clientPromise from '../../lib/mongodb';
 import { APP_CONFIG } from '../../lib/constants';
