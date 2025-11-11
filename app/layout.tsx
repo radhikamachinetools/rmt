da91cc2efd851faf2cfd55558fc61cdc66b445b0
ashokverma-an <ashokverma.an.2001@gmail.com>
2025-11-11 20:08:58 +0530

    Add favicon using company logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,9 +91,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/images/radhika-logo.png" sizes="any" />
+        <link rel="icon" href="/images/radhika-logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/images/radhika-logo.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
